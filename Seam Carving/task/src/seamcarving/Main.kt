c9faf9f491518be108e86794cea243421862bc3f
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-08-18 00:46:01 -0300

    Etapa 3/6

--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -1,21 +1,42 @@
 package seamcarving
 
 import java.awt.Color
+import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
+import kotlin.math.pow
+import kotlin.math.sqrt
 
-fun main(args:Array<String>) {
-    val inFile = File(args[1])
-    val outFile = File(args[3])
-    val inImage = ImageIO.read(inFile)
+fun main(args: Array<String>) {
+    val image = ImageIO.read(File(args[args.indexOf("-in") + 1]))
 
-    for (x in 0 until inImage.width) {
-        for (y in 0 until inImage.height) {
-            val color = Color(inImage.getRGB(x, y))
-            val newColor = Color(255 - color.red, 255 - color.green, 255 - color.blue)
-            inImage.setRGB(x, y, newColor.rgb)
+    val energies = mutableListOf<Double>()
+    for (x in 0 until image.width) {
+        for (y in 0 until image.height) {
+            energies.add(image.getEnergy(x, y))
+        }
+    }
+    val maxEnergy = energies.maxOf { it }
+
+    val newImage = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_RGB)
+    for (x in 0 until image.width) {
+        for (y in 0 until image.height) {
+            val intensity = (255.0 * image.getEnergy(x, y) / maxEnergy).toInt()
+            newImage.setRGB(x, y, Color(intensity, intensity, intensity).rgb)
         }
     }
 
-    ImageIO.write(inImage, "png", outFile)
+    ImageIO.write(newImage, "png", File(args[args.indexOf("-out") + 1]))
+}
+
+fun BufferedImage.getEnergy(x: Int, y: Int): Double {
+    val normalizedX = x.coerceAtLeast(1).coerceAtMost(this.width - 2)
+    val normalizedY = y.coerceAtLeast(1).coerceAtMost(this.height - 2)
+    val u = Color(this.getRGB(x, normalizedY - 1))
+    val r = Color(this.getRGB(normalizedX + 1, y))
+    val d = Color(this.getRGB(x, normalizedY + 1))
+    val l = Color(this.getRGB(normalizedX - 1, y))
+    val xGradient = (.0 + l.red - r.red).pow(2) + (.0 + l.green - r.green).pow(2) + (.0 + l.blue - r.blue).pow(2)
+    val yGradient = (.0 + u.red - d.red).pow(2) + (.0 + u.green - d.green).pow(2) + (.0 + u.blue - d.blue).pow(2)
+    return sqrt(xGradient + yGradient)
 }
