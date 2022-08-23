cc0e44dc4bf124870ce46da598e4a7520d1d6c5a
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-08-22 23:56:52 -0300

    Etapa 4/6 Encontre a costura

--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -4,29 +4,45 @@ import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
-import kotlin.math.pow
 import kotlin.math.sqrt
+import kotlin.math.pow
+
+
+data class SeamEnergy(val energy: Double, val xCoordinateInPreviousRow: Int = -1)
 
 fun main(args: Array<String>) {
     val image = ImageIO.read(File(args[args.indexOf("-in") + 1]))
 
-    val energies = mutableListOf<Double>()
-    for (x in 0 until image.width) {
-        for (y in 0 until image.height) {
-            energies.add(image.getEnergy(x, y))
+    val energies = mutableListOf<List<Double>>()
+    for (y in 0 until image.height) {
+        val energyRow = mutableListOf<Double>()
+        for (x in 0 until image.width) {
+            energyRow.add(image.getEnergy(x, y))
         }
-    }
-    val maxEnergy = energies.maxOf { it }
-
-    val newImage = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_RGB)
-    for (x in 0 until image.width) {
-        for (y in 0 until image.height) {
-            val intensity = (255.0 * image.getEnergy(x, y) / maxEnergy).toInt()
-            newImage.setRGB(x, y, Color(intensity, intensity, intensity).rgb)
-        }
+        energies.add(energyRow)
     }
 
-    ImageIO.write(newImage, "png", File(args[args.indexOf("-out") + 1]))
+    val seamEnergies = mutableListOf(energies[0].map { SeamEnergy(it) })
+    for (y in 1 until energies.size) {
+        val row = energies[y]
+        val seamEnergyRow = mutableListOf<SeamEnergy>()
+        for (x in row.indices) {
+            val xLeft = (x - 1).coerceAtLeast(0)
+            val xRight = (x + 1).coerceAtMost(row.size - 1)
+            val minAbovePos = (xLeft..xRight).minByOrNull { seamEnergies[y - 1][it].energy }!!
+            val minSeamEnergy = SeamEnergy(row[x] + seamEnergies[y - 1][minAbovePos].energy, minAbovePos)
+            seamEnergyRow.add(minSeamEnergy)
+        }
+        seamEnergies.add(seamEnergyRow)
+    }
+
+    var x = (0 until seamEnergies.last().size - 1).minByOrNull { seamEnergies.last()[it].energy }!!
+    for (y in seamEnergies.size - 1 downTo 0) {
+        image.setRGB(x, y, Color.RED.rgb)
+        x = seamEnergies[y][x].xCoordinateInPreviousRow
+    }
+
+    ImageIO.write(image, "png", File(args[args.indexOf("-out") + 1]))
 }
 
 fun BufferedImage.getEnergy(x: Int, y: Int): Double {
