28d45302e65a46622dac22d9c4536de9edaca811
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-08-23 23:00:45 -0300

    Etapa 5/6 Encontre a costura

--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -5,54 +5,62 @@ import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 import kotlin.math.sqrt
-import kotlin.math.pow
-
-
-data class SeamEnergy(val energy: Double, val xCoordinateInPreviousRow: Int = -1)
 
 fun main(args: Array<String>) {
-    val image = ImageIO.read(File(args[args.indexOf("-in") + 1]))
-
-    val energies = mutableListOf<List<Double>>()
-    for (y in 0 until image.height) {
-        val energyRow = mutableListOf<Double>()
-        for (x in 0 until image.width) {
-            energyRow.add(image.getEnergy(x, y))
-        }
-        energies.add(energyRow)
-    }
-
-    val seamEnergies = mutableListOf(energies[0].map { SeamEnergy(it) })
-    for (y in 1 until energies.size) {
-        val row = energies[y]
-        val seamEnergyRow = mutableListOf<SeamEnergy>()
-        for (x in row.indices) {
-            val xLeft = (x - 1).coerceAtLeast(0)
-            val xRight = (x + 1).coerceAtMost(row.size - 1)
-            val minAbovePos = (xLeft..xRight).minByOrNull { seamEnergies[y - 1][it].energy }!!
-            val minSeamEnergy = SeamEnergy(row[x] + seamEnergies[y - 1][minAbovePos].energy, minAbovePos)
-            seamEnergyRow.add(minSeamEnergy)
-        }
-        seamEnergies.add(seamEnergyRow)
-    }
-
-    var x = (0 until seamEnergies.last().size - 1).minByOrNull { seamEnergies.last()[it].energy }!!
-    for (y in seamEnergies.size - 1 downTo 0) {
-        image.setRGB(x, y, Color.RED.rgb)
-        x = seamEnergies[y][x].xCoordinateInPreviousRow
-    }
-
-    ImageIO.write(image, "png", File(args[args.indexOf("-out") + 1]))
+    val mainImage: BufferedImage = ImageIO.read(File(args[args.indexOf("-in") + 1]))
+    val transposedImg = transpose(mainImage)
+    drawVerticalSeam(transposedImg)
+    val horizontalSeam = transpose(transposedImg)
+    ImageIO.write(horizontalSeam, "png", File(args[args.indexOf("-out") + 1]))
 }
 
-fun BufferedImage.getEnergy(x: Int, y: Int): Double {
-    val normalizedX = x.coerceAtLeast(1).coerceAtMost(this.width - 2)
-    val normalizedY = y.coerceAtLeast(1).coerceAtMost(this.height - 2)
-    val u = Color(this.getRGB(x, normalizedY - 1))
-    val r = Color(this.getRGB(normalizedX + 1, y))
-    val d = Color(this.getRGB(x, normalizedY + 1))
-    val l = Color(this.getRGB(normalizedX - 1, y))
-    val xGradient = (.0 + l.red - r.red).pow(2) + (.0 + l.green - r.green).pow(2) + (.0 + l.blue - r.blue).pow(2)
-    val yGradient = (.0 + u.red - d.red).pow(2) + (.0 + u.green - d.green).pow(2) + (.0 + u.blue - d.blue).pow(2)
-    return sqrt(xGradient + yGradient)
+fun transpose(image: BufferedImage): BufferedImage {
+    val newImage = BufferedImage(image.height, image.width, BufferedImage.TYPE_INT_RGB)
+    with (image) {
+        for (x in 0 until width)
+            for (y in 0 until height) {
+                val color = image.getRGB(x, y)
+                newImage.setRGB(y, x, color)
+            }
+    }
+    return newImage
+}
+
+fun drawVerticalSeam(image: BufferedImage, color: Color = Color.red) {
+    fun pow2(n: Int) = n * n
+    with (image) {
+        val energies = Array(height) { Array(width) { 0.0 } }
+        for (y in 0 until height)
+            for (x in 0 until width){
+                val cx = x.coerceIn(1..width - 2)
+                val cy = y.coerceIn(1..height - 2)
+                val west = Color(getRGB(cx - 1, y))
+                val east = Color(getRGB(cx + 1, y))
+                val north = Color(getRGB(x, cy - 1))
+                val south = Color(getRGB(x, cy + 1))
+                val gradientX = pow2(west.red - east.red) + pow2(west.green - east.green) + pow2(west.blue - east.blue)
+                val gradientY = pow2(north.red - south.red) + pow2(north.green - south.green) + pow2(north.blue - south.blue)
+                energies[y][x] = sqrt((gradientX + gradientY).toDouble())
+            }
+
+        val weights = Array(height) { Array(width) { 0.0 } }
+        weights[0] = energies[0].copyOf()
+        for (y in 1 until height) {
+            for (x in 0 until width) {
+                var minimo = weights[y - 1][x]
+                if (x > 0 && minimo > weights[y - 1][x - 1]) minimo = weights[y - 1][x - 1]
+                if (x < width - 1 && minimo > weights[y - 1][x + 1]) minimo = weights[y - 1][x + 1]
+                weights[y][x] = energies[y][x] + minimo
+            }
+        }
+        var idx = 0
+        for (x in 1 until width - 1) if (weights[height - 1][idx] > weights[height - 1][x]) idx = x
+        setRGB(idx, height - 1, color.rgb)
+        for (y in height - 2 downTo 0) {
+            for (x in (idx - 1).coerceAtLeast(0)..(idx + 1).coerceAtMost(width - 1))
+                if (weights[y + 1][idx] == energies[y + 1][idx] + weights[y][x]) idx = x
+            setRGB(idx, y, color.rgb)
+        }
+
+    }
 }
